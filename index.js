2529177430de042e05f3e6202f2cf6998d76774f
Ian <ssianfilipe@gmail.com>
2022-08-24 11:07:52 -0300

    returning links

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import chalk from "chalk";
 import fs from "fs";
 
+function extractLinks(text) {
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+  const resultArr = [];
+  let temp;
+  while ((temp = regex.exec(text)) !== null) {
+    resultArr.push({ [temp[1]]: temp[2] });
+  }
+  return resultArr;
+}
+
 function handleError(error) {
   throw new Error(chalk.red(error));
 }
@@ -9,7 +19,7 @@ async function getFile(filePath) {
   const encoding = "utf-8";
   try {
     const text = await fs.promises.readFile(filePath, encoding);
-    console.log(chalk.green(text));
+    console.log(extractLinks(text));
   } catch (err) {
     handleError(err);
   }
