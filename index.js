50496da547d4a57a9a196676283be5261c14b637
Ian <ssianfilipe@gmail.com>
2022-08-24 09:26:39 -0300

    promises

--- index.js
+++ index.js
@@ -7,12 +7,20 @@ function handleError(error) {
 
 function getFile(filePath) {
   const encoding = "utf-8";
-  fs.readFile(filePath, encoding, (err, text) => {
-    if (err) {
-      handleError(err);
-    }
-    console.log(chalk.green(text));
-  });
+  fs.promises
+    .readFile(filePath, encoding)
+    .then((text) => console.log(chalk.green(text)))
+    .catch((err) => handleError(err));
 }
+
+// function getFile(filePath) {
+//   const encoding = "utf-8";
+//   fs.readFile(filePath, encoding, (err, text) => {
+//     if (err) {
+//       handleError(err);
+//     }
+//     console.log(chalk.green(text));
+//   });
+// }
 
 getFile("./files/text1.md");
