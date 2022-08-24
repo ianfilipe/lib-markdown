20ec028a9c0ede315ae5b5dc7af232aa23dcbafa
Ian <ssianfilipe@gmail.com>
2022-08-24 09:20:24 -0300

    handling error

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import chalk from "chalk";
 import fs from "fs";
 
+function handleError(error) {
+  throw new Error(chalk.red(error));
+}
+
 function getFile(filePath) {
   const encoding = "utf-8";
   fs.readFile(filePath, encoding, (err, text) => {
+    if (err) {
+      handleError(err);
+    }
     console.log(chalk.green(text));
   });
 }
