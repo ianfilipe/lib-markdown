2b84dc5c9c70fa944fe2f1ad22eebcdf33d67d40
Ian <ssianfilipe@gmail.com>
2022-08-24 09:10:44 -0300

    import fs and chalk

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+import chalk from "chalk";
+import fs from "fs";
+
+function getFile(filePath) {
+  const encoding = "utf-8";
+  fs.readFile(filePath, encoding, (err, text) => {
+    console.log(chalk.green(text));
+  });
+}
+
+getFile("./files/text1.md");
